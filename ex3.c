8ab92ef7dffdcf6f1b229a73ee143a38e891e9b5
xtrntr <kangren.chia@gmail.com>
2016-03-21 14:09:45 +0800

    ex3 finished
    naive and hacky ranking system, but it works. the top 2nd result is the
    answer

--- ex3.c
+++ ex3.c
@@ -53,6 +53,7 @@ void single_byte_xor(char *str, char *byte_cipher, char *res)
     }
 }
 
+// still not very clear how this bit shifting magic works
 void num2bin_byte(int num, char *str)
 {
   *(str+9) = '\0';
@@ -91,6 +92,140 @@ void bin2ascii(char *bin, char *res)
     }
 }
 
+//crappy scoring, but whatever works
+float score(char *str)
+{
+  float acc = 0;
+  for (int i = 0 ; i < strlen(str) ; i++)
+    {
+      switch(str[i])
+        {
+        case 'e' : 
+          acc+=12.7;
+        case 't' : 
+          acc+=9;
+        case 'a' : 
+          acc+=8.2;
+        case 'o' : 
+          acc+=7.5;
+        case 'i' : 
+          acc+=7;
+        case 'n' : 
+          acc+=6.7;
+        case 's' : 
+          acc+=6.3;
+        case 'h' : 
+          acc+=6;
+        case 'r' : 
+          acc+=6;
+        case 'd' : 
+          acc+=4.3;
+        case 'l' : 
+          acc+=4;
+        case 'c' : 
+          acc+=2.7;
+        case 'u' : 
+          acc+=2.7;
+        case 'm' : 
+          acc+=2.4;
+        case 'w' : 
+          acc+=2.3;
+        case 'f' : 
+          acc+=2.2;
+        case 'y' : 
+          acc+=2;
+        case 'p' : 
+          acc+=1.9;
+        case 'b' : 
+          acc+=1.5;
+        case 'v' : 
+          acc+=1;
+        case 'k' : 
+          acc+=0.9;
+        case 'j' : 
+          acc+=0.7;
+        case 'x' : 
+          acc+=0.2;
+        case 'q' : 
+          acc+=0.2;
+        case 'z' : 
+          acc+=0.1;
+        case 'E' : 
+          acc+=12.7;
+        case 'T' : 
+          acc+=9;
+        case 'A' : 
+          acc+=8.2;
+        case 'O' : 
+          acc+=7.5;
+        case 'I' : 
+          acc+=7;
+        case 'N' : 
+          acc+=6.7;
+        case 'S' : 
+          acc+=6.3;
+        case 'H' : 
+          acc+=6;
+        case 'R' : 
+          acc+=6;
+        case 'D' : 
+          acc+=4.3;
+        case 'L' : 
+          acc+=4;
+        case 'C' : 
+          acc+=2.7;
+        case 'U' : 
+          acc+=2.7;
+        case 'M' : 
+          acc+=2.4;
+        case 'W' : 
+          acc+=2.3;
+        case 'F' : 
+          acc+=2.2;
+        case 'Y' : 
+          acc+=2;
+        case 'P' : 
+          acc+=1.9;
+        case 'B' : 
+          acc+=1.5;
+        case 'V' : 
+          acc+=1;
+        case 'K' : 
+          acc+=0.9;
+        case 'J' : 
+          acc+=0.7;
+        case 'X' : 
+          acc+=0.2;
+        case 'Q' : 
+          acc+=0.2;
+        case 'Z' : 
+          acc+=0.1;
+        case ' ' : 
+          acc+=5;
+        default :
+          acc-=25;
+        }
+    }
+  return acc;
+}
+
+struct possibility
+{
+  char *msg;
+  float score;
+};
+
+int compare(const void *s1, const void *s2)
+{
+  struct possibility *e1 = (struct possibility *)s1;
+  struct possibility *e2 = (struct possibility *)s2;
+  int scorecompare  = e1->score - e2->score;
+  if (scorecompare == 0)  // same score so heck care
+    return -1;
+  else
+    return -scorecompare;
+}
+
 int main()
 {
   char* hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
@@ -98,18 +233,27 @@ int main()
   char bin1[1024] = {};
   char res[1024] = {};
   char byte[9] = {};
+  char *msg;
+  struct possibility possibilities[256];
 
   hex2bin_str(hex, bin1);
   for (int i = 0 ; i < 256 ; i++)
     {
+      msg = malloc(1024);
       num2bin_byte(i, byte);
       single_byte_xor(bin1, byte, res);
-      char a[1024] = {};
-      bin2ascii(res, a);
-      printf("decoded msg %s using cipher %s \n", a, byte);
+      bin2ascii(res, msg);
+      possibilities[i].score = score(msg);
+      possibilities[i].msg = msg;
     }
-  /*
-  */
+  
+  qsort(possibilities, 256, sizeof(struct possibility), compare);
+
+  //print the top 10 values only
+  for (int i = 0 ; i < 10 ; i++)
+    {
+      printf("msg = %s, score = %f \n", possibilities[i].msg, possibilities[i].score);
+    }
 
   return 0;
 }
